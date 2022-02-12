b82eb958c7664887ce460f308908c6bedc689633
Andrey Rumakin <rumakin.andrey@yandex.ru>
2022-02-12 12:49:22 +0400

    Add src/locales/

---
+++ src/locales/en.js
@@ -0,0 +1,25 @@
+export default {
+  translation: {
+    h1Text: 'RSS aggregator',
+    headText: "Start reading RSS today! It's easy, it's beautiful.",
+    rssLabel: 'link RSS',
+    rssExample: 'Example: https://ru.hexlet.io/lessons.rss',
+    buttonAdd: 'Add',
+    feedback: {
+      invalidLink: 'The link must be a valid URL',
+      notValidRss: 'The resource does not contain valid RSS',
+      duplicate: 'RSS already exists',
+      success: 'RSS uploaded successfully',
+      networkError: 'Network error',
+    },
+    posts: {
+      post: 'Posts',
+      button: 'Viewing',
+    },
+    feeds: 'Feeds',
+    buttonModal: {
+      primary: 'Read completely',
+      secondary: 'Close',
+    },
+  },
+};
---
+++ src/locales/ru.js
@@ -0,0 +1,25 @@
+export default {
+  translation: {
+    h1Text: 'RSS агрегатор',
+    headText: 'Начните читать RSS сегодня! Это легко, это красиво.',
+    rssLabel: 'ссылка RSS',
+    rssExample: 'Пример: https://ru.hexlet.io/lessons.rss',
+    buttonAdd: 'Добавить',
+    feedback: {
+      invalidLink: 'Ссылка должна быть валидным URL',
+      notValidRss: 'Ресурс не содержит валидный RSS',
+      duplicate: 'RSS уже существует',
+      success: 'RSS успешно загружен',
+      networkError: 'Ошибка сети',
+    },
+    posts: {
+      post: 'Посты',
+      button: 'Просмотр',
+    },
+    feeds: 'Фиды',
+    buttonModal: {
+      primary: 'Читать полностью',
+      secondary: 'Закрыть',
+    },
+  },
+};
